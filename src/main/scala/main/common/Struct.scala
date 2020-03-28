717fb61f202974310c0261e011ac2fb0d92a210f
radek <ferdudas97@gmail.com>
2020-03-28 21:42:48 +0100

    new csv columns

--- src/main/scala/main/common/Struct.scala
+++ src/main/scala/main/common/Struct.scala
@@ -5,7 +5,7 @@ case class Series(seriesCode: String, topic: String, indicatorName: String, aggr
 case class Indicator(countryCode: String, indicatorCode: String, year: Int, value: Double)
 
 
-case class Country(countryCode: String, shortName: String)
+case class Country(countryCode: String, shortName: String, incomeGroup: String, region: String)
 
 
 case class LifeExp(country: String, year: Int, lifeExpectancy: Double, gdp: Double)
